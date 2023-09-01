b272597d2f9441888aeb5fbba6c9767fb3d96a3f
Preetham Ramesh <preethamramesh99@gmail.com>
2023-09-01 16:54:08 -0700

    did more sign up workflow

--- Backend/routes/auth.py
+++ Backend/routes/auth.py
@@ -7,6 +7,11 @@ def sign_in_route():
     return sign_in_service.sign_in()
 
 sign_up = Blueprint('sign_up', __name__)
-@sign_up.route('/sign_up')
+@sign_up.route('/sign_up', methods=['POST'])
 def sign_up_route():
-    return sign_up_service.sign_up()
+    try:
+        # parse http request
+        return sign_up_service.sign_up()
+    except:
+        # return http status code and error message
+        return "Error during sign up"
