025f038f04bf0706ec609351bd9045b70486f3fa
Preetham Ramesh <preethamramesh99@gmail.com>
2023-09-05 01:00:11 -0700

    added error handling for sign up service

--- Backend/services/authentication/sign_up_service.py
+++ Backend/services/authentication/sign_up_service.py
@@ -4,7 +4,9 @@ class SignUpService:
         self.phoneNum = body['phoneNum']
         self.username = body['username']
         self.password = body['password']
+        self.salt = body['salt']
         self.name = body['name']
+        self.profilePic = body['profilePic']
 
     def sign_up(self):
         if self.email != '' and self.phoneNum == '' and self.username == '':
@@ -18,10 +20,20 @@ class SignUpService:
         if self.email == '' and self.phoneNum == '' and self.username != '':
             # make db call
             return ['checking database for username', 200]
-
+        
         if self.email != '' and self.phoneNum != '' and self.username != '' and self.password != '' and self.name != '':
-            # make db call
+            #send above info + salt, name and profile pic to db in try catch
             return ['inserting user data into db', 201]
+        else:
+            missing = []
+            attrs = dir(self)
+            attrs = [attr for attr in attrs if not attr.startswith('__')]
+            for attr in attrs:
+                val = getattr(self, attr)
+                if val == '':
+                    missing.append(attr)
+
+            return [f'Empty fields: {missing}', 400]
         
         return ['general error', 500] # try to remove
     
