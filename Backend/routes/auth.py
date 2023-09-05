4df97ffe81087756046ada040cf6b0ec00e084a7
arshmalhotra <arshmalhotra@users.noreply.github.com>
2023-09-05 03:42:24 -0700

    Removing completed TODO comment and fixing function return

--- Backend/routes/auth.py
+++ Backend/routes/auth.py
@@ -5,9 +5,6 @@ from services.authentication.sign_in_service import SignInService
 get_sign_in_challenge = Blueprint('get_sign_in_challenge', __name__)
 @get_sign_in_challenge.route('/get_sign_in_challenge', methods=['POST'])
 def get_sign_in_challenge_route():
-    ############
-    # switch to request.form for POST
-    ############
     req_body = request.form
     if not req_body:
         return make_response('Request Error: No request body provided.', 400)
@@ -28,7 +25,7 @@ def get_sign_in_challenge_route():
 sign_in = Blueprint('sign_in', __name__)
 @sign_in.route('/sign_in')
 def sign_in_route():
-
+    pass
 
 sign_up = Blueprint('sign_up', __name__)
 @sign_up.route('/sign_up', methods=['POST'])
